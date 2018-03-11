5b5a1df5c916319bfc78b9787264160de85f5c8c
tdietert <thomas@adjoint.io>
2018-03-11 11:45:05 +0000

    Reorganize config read/parse & cleanup main

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,65 +1,18 @@
 
 module Main where
 
-import Protolude hiding (option)
-
-import Config (Config(..), readConfig)
-import Nanocoin (initNode)
-import Nanocoin.Network.Utils (RPCPort, P2PPort)
+import Protolude
 
 import Options.Applicative
-import Network.Socket (HostName, PortNumber)
 
-fallback :: Parser (Maybe a) -> a -> Parser a
-fallback parser x = fallback' x <$> parser
-  where
-    fallback' (Just x) _ = x
-    fallback' Nothing x  = x
+import Config (readConfig, parseConfig)
+import Nanocoin (initNode)
 
 main :: IO ()
 main = do
     -- Read config from config file
     defaultConfig <- readConfig "config"
     -- Override config with cmd line arguments
-    config <- execParser $
-      info (parseConfig defaultConfig) mempty
+    config <- execParser $ info (parseConfig defaultConfig) mempty
+    -- Boot the nanocoin node
     initNode config
-  where
-    parseConfig config = Config
-      <$> hostnameParser `fallback` (hostname config)
-      <*> rpcPortParser  `fallback` (rpcPort config)
-      <*> p2pPortParser  `fallback` (p2pPort config)
-      <*> pure (bootnodes config) -- XXX add cmd line option for adding bootnodes
-      <*> keysParser
-      <*> logFileParser
-
-    hostnameParser :: Parser (Maybe HostName)
-    hostnameParser = optional $
-      option auto $ long "hostname"
-                 <> metavar "HOSTNAME"
-
-    rpcPortParser :: Parser (Maybe RPCPort)
-    rpcPortParser = optional $
-      option auto $ long "rpc-port"
-                 <> short 'p'
-                 <> metavar "RPC_PORT"
-
-    p2pPortParser :: Parser (Maybe P2PPort)
-    p2pPortParser =
-      fmap (fmap fromIntegral) $
-        optional $ option auto $
-             long "p2p-port"
-          <> short 'n'
-          <> metavar "P2P_PORT"
-
-    keysParser :: Parser (Maybe FilePath)
-    keysParser = optional $
-      strOption $ long "keys"
-               <> short 'k'
-               <> metavar "KEYS_DIR"
-
-    logFileParser :: Parser (Maybe FilePath)
-    logFileParser = optional $
-      strOption $ long "logfile"
-              <> short 'f'
-              <> metavar "LOG_FILE"
--- src/Config.hs
+++ src/Config.hs
@@ -1,10 +1,13 @@
 
 module Config where
 
-import Protolude
+import Protolude hiding (option)
+
+import Options.Applicative
 
 import qualified Data.Configurator as C
 import qualified Data.Configurator.Types as C
+
 import Network.Socket (HostName, PortNumber)
 
 import Nanocoin.Network.Utils (RPCPort, P2PPort)
@@ -20,6 +23,7 @@ data Config = Config
   , logFilePath  :: Maybe FilePath   -- ^ Filepath to log to
   }
 
+-- Read config from config file
 readConfig :: FilePath -> IO Config
 readConfig cfgFile = do
   cfg <- C.load [C.Required cfgFile]
@@ -42,3 +46,49 @@ readConfig cfgFile = do
 
     intToP2PPort :: Int -> P2PPort
     intToP2PPort = fromIntegral
+
+-- Parse Config options via command line
+parseConfig config = Config
+    <$> hostnameParser `fallback` (hostname config)
+    <*> rpcPortParser  `fallback` (rpcPort config)
+    <*> p2pPortParser  `fallback` (p2pPort config)
+    <*> pure (bootnodes config) -- XXX add cmd line option for adding bootnodes
+    <*> keysParser
+    <*> logFileParser
+  where
+    hostnameParser :: Parser (Maybe HostName)
+    hostnameParser = optional $
+      option auto $ long "hostname"
+                 <> metavar "HOSTNAME"
+
+    rpcPortParser :: Parser (Maybe RPCPort)
+    rpcPortParser = optional $
+      option auto $ long "rpc-port"
+                 <> short 'p'
+                 <> metavar "RPC_PORT"
+
+    p2pPortParser :: Parser (Maybe P2PPort)
+    p2pPortParser =
+      fmap (fmap fromIntegral) $
+        optional $ option auto $
+             long "p2p-port"
+          <> short 'n'
+          <> metavar "P2P_PORT"
+
+    keysParser :: Parser (Maybe FilePath)
+    keysParser = optional $
+      strOption $ long "keys"
+               <> short 'k'
+               <> metavar "KEYS_DIR"
+
+    logFileParser :: Parser (Maybe FilePath)
+    logFileParser = optional $
+      strOption $ long "logfile"
+              <> short 'f'
+              <> metavar "LOG_FILE"
+
+fallback :: Parser (Maybe a) -> a -> Parser a
+fallback parser x = flip fallback' x <$> parser
+  where
+    fallback' (Just x) _ = x
+    fallback' Nothing x  = x
